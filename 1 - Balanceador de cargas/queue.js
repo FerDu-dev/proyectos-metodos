f79d72e0535dfbe88f18a407175a1fc70805595c
Santiago <Santiagogbf2000@Gmail.com>
2023-08-18 10:27:29 -0400

    Monorepo multipackage inicializado

--- 1 - Balanceador de cargas/queue.js
+++ 1 - Balanceador de cargas/queue.js
@@ -1,77 +1,71 @@
 // Clase Queue los métodos tradicionales
-export default class Queue{
+export default class Queue {
+  constructor () {
+    this.items = []
+  }
 
-    constructor(){
-    this.items = [];
+  enqueue (element) {
+    this.items.push(element)
+  }
+
+  isEmpty () {
+    return this.items.length === 0
+  }
+
+  dequeue () {
+    if (this.isEmpty()) {
+      return 'No element in Queue'
     }
-    
-    enqueue(element) {
-        this.items.push(element)
+    return this.items.shift()
+  }
+
+  front () {
+    if (this.isEmpty()) {
+      return 'No elements in Queue'
     }
+    return this.items[0]
+  }
 
-    isEmpty() {
-        return this.items.length === 0;
+  printQueue () {
+    let str = ''
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + ''
     }
+    return str
+  }
 
-    dequeue(){
-        if(this.isEmpty()){
-            return "No element in Queue"
-        }
-        return this.items.shift();
+  size () {
+    return this.items.length
+  }
+
+  clear () {
+    this.items = []
+  }
+
+  reverse () {
+    return this.items.reverse()
+  }
+
+  peek () {
+    return this.items[this.items.length - 1]
+  }
+
+  search (element) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === element) {
+        return true
+      }
     }
+    return false
+  }
 
-    front(){
-        if(this.isEmpty()){
-            return "No elements in Queue"
-        }
-        return this.items[0];
+  remove (element) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === element) {
+        this.items.splice(i, 1)
+        return true
+      }
     }
-
-    printQueue(){
-        var str = "";
-        for(var i = 0; i < this.items.length; i++){
-            str += this.items[i] + "";
-        }
-        return str
-    }
-
-
-    size(){
-        return this.items.length;
-    }
-
-    clear(){
-        this.items = [];
-    }
-
-    reverse(){
-        return this.items.reverse();
-    }
-
-    peek(){
-        return this.items[this.items.length - 1];
-    }
-
-    search(element){
-        for(var i = 0; i < this.items.length; i++){
-            if(this.items[i] == element){
-                return true 
-            }
-        }
-        return false
-    }
-
-    remove(element){
-        for(var i = 0; i < this.items.length; i++){
-            if(this.items[i] == element){
-                this.items.splice(i, 1);
-                return true
-            }
-        }
-        return false
-    }
-
+    return false
+  }
 }
-
-    
-
