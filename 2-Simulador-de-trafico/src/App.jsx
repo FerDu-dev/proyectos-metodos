fc67e115ca3ba45ffc70d256caad20c2467ff808
andrespausin <111588426+andrespausin@users.noreply.github.com>
2023-08-30 20:59:32 -0400

    diseno fontEnd simulacion de trafico

--- 2-Simulador-de-trafico/src/App.jsx
+++ 2-Simulador-de-trafico/src/App.jsx
@@ -1,8 +1,21 @@
+import { controlador } from './services/Controllers.js'
 function App () {
   return (
     <>
-      <div>
-        <div />
+    <h1>HOLA MI NOMBRE ANDRES</h1>
+      <div className="container">
+        <div className='container-block'>
+
+        </div>
+        <div className="container-block"> 
+          
+        </div>
+        <div className="container-block">
+
+        </div>
+        <div className="container-block">
+
+        </div>
       </div>
     </>
   )
---
+++ 2-Simulador-de-trafico/src/services/Controllers.js
@@ -0,0 +1,15 @@
+import { SemaforoCompuesto } from '../Logic/Class/Semaforo'
+import { ControladorCompuesto } from '../Logic/Class/Controlador.js'
+import { InterseccionCompuesta } from '../Logic/Class/Interseccion.js'
+
+const semaforo1 = new SemaforoCompuesto()
+const semaforo2 = new SemaforoCompuesto()
+const semaforo3 = new SemaforoCompuesto()
+const semaforo4 = new SemaforoCompuesto()
+
+const interseccion1 = new InterseccionCompuesta([[semaforo1, semaforo3], [semaforo2, semaforo4]])
+
+const controlador = new ControladorCompuesto(interseccion1)
+controlador.sincronizarSemaforos([[16, 8], [12, 6]])
+
+export {controlador}
