1186a1c97a05ad868ffd1f833114acf3ae6ece96
andrespausin <111588426+andrespausin@users.noreply.github.com>
2023-09-03 21:48:31 -0400

    Se agregaron estilos al proyecto de simulador de trafico

--- 2-Simulador-de-trafico/src/App.jsx
+++ 2-Simulador-de-trafico/src/App.jsx
@@ -20,12 +20,23 @@ function App () {
   }, [start])
 
   return (
-    <main className='app'>
-      <Container />
-      <button onClick={toggleSimulation}>
-        {start ? 'Detener' : 'Comenzar'} simulación
-      </button>
+    <main className='app contenedor'>
+      <div className='app'>
+        <h1>Simulador de Tráfico</h1>
+        <Container />
+        <button className='button-56' onClick={toggleSimulation}>
+          {start ? 'Detener' : 'Comenzar'} simulación
+        </button>
+        </div>
+        <div className='integrantes'>
+          <p>Santiago Briceño 28.063.634</p>
+          <p>César Pausín 28.402.367</p>
+          <p>Fernando Duno 27.173.917</p>
+          <p>José Quintero 27.657.168</p>
+          <p>Sofía Tamaris 30.053.436</p>
+      </div>
     </main>
+    
   )
 }
 
